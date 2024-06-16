eb11d5e6c2de8c214ac52a32acb4e69f47f1754e
Arthur Bargas <arthurbargas.ab@gmail.com>
2024-06-16 15:51:59 +0200

    portfolio fonctionnel

--- src/App.js
+++ src/App.js
@@ -5,16 +5,14 @@ import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
-import logo from './logo.svg';
-import './App.css';
 
 export default function App() {
   return (
     <main className='text-gray-400 bg-gray-900 body-font'>
       <Navbar/>
       <About/>
-      <Projects/>
       <Skills/>
+      <Projects/>
       <Testimonials/>
       <Contact/>
     </main>
